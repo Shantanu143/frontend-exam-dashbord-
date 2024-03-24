00830c9585fee7a24f5bd7c1d85c1d613cc6c6bf
Rahul kardile <rahulkardile321@gmail.com>
2024-03-24 19:56:52 +0530

    backend user update

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import User from "./Routes/UserRoute.js"
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3300;
+const MONGO_URL = process.env.MONGO_URL;
+
+try {
+    mongoose.connect(MONGO_URL)
+    console.log("Database is connected");
+} catch (error) {
+    console.log("Database is Not Connected!");
+    console.log(error);
+}
+
+app.get("/api/test", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "api is working"
+    })
+})
+
+app.use("/api/user", User);
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on ${PORT}`);
+})
